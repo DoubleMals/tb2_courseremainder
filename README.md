1fdbb3e9083c1959ba915326fc5f0b32bf7208c4
Barra Putra <barraputra45@gmail.com>
2023-12-20 21:33:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# tb2_courseremainder
+
+Implementasi SQF Lite Atau Hive No SQL CRUD pada flutter
+
+Muhammad Barra Putra Hananto
+41823010131
---
+++ android/app/src/main/kotlin/com/example/tb2_courseremainder/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tb2_courseremainder
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
